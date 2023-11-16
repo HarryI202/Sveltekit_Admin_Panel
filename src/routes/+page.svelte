77b93bf4cf628f22dd9d248b54f71819c6c954fb
Haraprasad Evince <haraprasad@evincedev.com>
2023-11-16 16:57:10 +0530

    added iframe

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,11 +32,20 @@
             </p>
           </div>
           <div class="card-body">
-            <div class="mapcontainer">
-              <div class="map">
+            <!-- <div class="mapcontainer"> -->
+              <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63197946.13199194!2d36.08692214893822!3d14.808607335921637!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1700133449494!5m2!1sen!2sin"
+              width="100%"
+              height="300"
+              style="border:0;"
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+              title="India"
+            />
+              <div class="map hidden">
                 <span>Alternative content for the map</span>
               </div>
-            </div>
+            <!-- </div> -->
           </div>
         </div>
       </div>
